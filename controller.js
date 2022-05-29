7dbc9095f385eb0fb1d4e8b78d8e5bdf8f3adad0
aryawardhana <wardhana2451@gmail.com>
2022-05-29 16:23:58 +0700

    showing mahasiswa based on id_mahasiswa

--- controller.js
+++ controller.js
@@ -11,10 +11,24 @@ exports.index = function (req, res){
 exports.tampilsemuamahasiswa = function(req,res){
         connection.query('SELECT * FROM mahasiswa' , function(errors, rows , fields){
             if(errors){
-                connection.log(errors);
+                console.log(errors);
             }
             else{
                 response.ok(rows, res)
             }
         });
 };  
+// menampilkan mahasiswa berdasarkan ID 
+exports.tampilkanmahasiswaid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?',[id],
+        function(errors, rows , fields){
+            if(errors){
+                console.log(errors);
+            }
+            else{
+                response.ok(rows, res)
+            }
+        }
+    )
+}
