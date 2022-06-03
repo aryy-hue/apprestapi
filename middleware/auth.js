00ec6dc0f4a119307e6b84d4914f983c9039fe88
aryawardhana <wardhana2451@gmail.com>
2022-06-03 14:46:45 +0700

    add login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,7 @@ var response    = require('../res');
 var jwt        = require('jsonwebtoken');
 var config     = require('../config/secret');
 var ip         = require('ip');
+const { connect } = require('../koneksi');
 
 
 // controller untuk register
@@ -44,4 +45,55 @@ exports.registrasi  = function(req,res){
             }
         }
     })
+}
+// controleler untuk login
+exports.login = function(req,res){
+    var post ={
+        password: req.body.password ,
+        email: req.body.email
+    }
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["user","password",md5(post.password), "email",post.email];
+
+    query = mysql.format(query,table);
+    connection.query(query,function(errors,rows){
+        if(errors){
+            console.log(errors);
+        }else{
+            if(rows.length ==1){
+                var token = jwt.sign({rows},config.secret,{
+                    expiresIn: 1440
+                });
+                id_user = rows[0].id;
+
+                var data={
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address:ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table =["akses_token"];
+
+                query = mysql.format(query, table);
+                connection.query(query,data,function(errors,rows){
+                    if(errors){
+                        console.log(errors);
+                    }else{
+                        res.json({
+                            success:true,
+                            message:"Token JWT tergenerate",
+                            token:token,
+                            curUser:data.id_user
+                        });
+                    }
+                });
+            }else{
+                res.json({
+                    "Error":true,
+                    "Message":"Email atau password salah"
+                });
+            }
+        }
+    });
 }
